3ed1d7a5c27c8be5bfba67b229d757cf3678f5b6
Rinku <rinkudas.id@gmail.com>
2024-05-09 22:01:10 +0530

    Added condition to pass test case #10 when input string is invalid

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,7 @@
 class StringCalculator
   def add(string)
     return 0 if string.empty? || string.nil?
+    return 'Invalid' if string.include?('\n') && string.match?(/[0-9],\\n/)
     default_delimeter = ','
     delimeter = string.start_with?('//') ? string[2] : default_delimeter
     numbers = string.split(/\\n|#{delimeter}/).map(&:to_i)
