32b2dfded370a3397a8d52809942c2f40e29a031
Rinku <rinkudas.id@gmail.com>
2024-05-09 13:31:13 +0530

    Added test case #2 to check if add method returns 0 for empty string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,5 +1,6 @@
 require 'string_calculator'
 RSpec.describe StringCalculator do
+  let(:string_calculator) { StringCalculator.new }
 
   it 'creates a string_calculator class' do
     string_calculator = StringCalculator.new
@@ -9,6 +10,14 @@ RSpec.describe StringCalculator do
   it 'has a add method' do
     string_calculator = StringCalculator.new
     expect(string_calculator).to respond_to(:add)
+  end
+
+  describe 'add' do
+    context 'when input is empty string' do
+      it 'should return 0' do
+        expect(string_calculator.add('')).to eq 0
+      end
+    end
   end
   
 end
