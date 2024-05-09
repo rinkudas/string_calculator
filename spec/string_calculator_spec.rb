269e92ed676a193c38f248e8f376d6e0764a03c8
Rinku <rinkudas.id@gmail.com>
2024-05-09 21:10:49 +0530

    Added test case #8 when input string has different delimeter defined

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -40,6 +40,12 @@ RSpec.describe StringCalculator do
         expect(string_calculator.add('1\n2,3')).to eq 6
       end
     end
+
+    context 'when input string has different delimeter' do
+      it 'should return the sum of all numbers' do
+        expect(string_calculator.add('//;\n1;2')).to eq 3
+      end
+    end
   end
   
 end
