de92fb47e30e75837ac2f9b180073aa0fd66977e
Rinku <rinkudas.id@gmail.com>
2024-05-09 20:01:16 +0530

    Added condition to pass test case #5 for two numbers string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,5 +2,10 @@ class StringCalculator
   def add(string)
     return 0 if string.empty?
     return string.to_i if string.length == 1
+
+    if string.length >= 2
+      num1, num2 = string.split(',')
+      return num1.to_i + num2.to_i
+    end
   end
 end
