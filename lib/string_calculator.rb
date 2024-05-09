35c7cd1ac318ff7126457a5a62b86f77c37b0905
Rinku <rinkudas.id@gmail.com>
2024-05-09 21:15:14 +0530

    Added condition to pass test case #8 when input string has different delimeter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,8 @@
 class StringCalculator
   def add(string)
     return 0 if string.empty? || string.nil?
-    return string.split(/\\n|,/).map(&:to_i).inject(:+)
+    default_delimeter = ','
+    delimeter = string.start_with?('//') ? string[2] : default_delimeter
+    return string.split(/\\n|#{delimeter}/).map(&:to_i).inject(:+)
   end
 end
