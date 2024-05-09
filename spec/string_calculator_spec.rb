d2cf33d9c0e44458e6269b765ab101a72eacd166
Rinku <rinkudas.id@gmail.com>
2024-05-09 19:43:36 +0530

    Added test case #4 for single number string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,12 +3,10 @@ RSpec.describe StringCalculator do
   let(:string_calculator) { StringCalculator.new }
 
   it 'creates a string_calculator class' do
-    string_calculator = StringCalculator.new
     expect(string_calculator).to be_kind_of(StringCalculator)
   end
 
   it 'has a add method' do
-    string_calculator = StringCalculator.new
     expect(string_calculator).to respond_to(:add)
   end
 
@@ -16,6 +14,12 @@ RSpec.describe StringCalculator do
     context 'when input is empty string' do
       it 'should return 0' do
         expect(string_calculator.add('')).to eq 0
+      end
+    end
+
+    context 'when input is single number string' do
+      it 'should return the same number' do
+        expect(string_calculator.add('1')).to eq 1
       end
     end
   end
