5b6595ec3201f9cfc8bb5c9be81a91b4c8b551a2
Rinku <rinkudas.id@gmail.com>
2024-05-09 21:31:13 +0530

    Added condition to pass test case #9 when input string has negative numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,6 +3,9 @@ class StringCalculator
     return 0 if string.empty? || string.nil?
     default_delimeter = ','
     delimeter = string.start_with?('//') ? string[2] : default_delimeter
-    return string.split(/\\n|#{delimeter}/).map(&:to_i).inject(:+)
+    numbers = string.split(/\\n|#{delimeter}/).map(&:to_i)
+    negative_nums = numbers.select(&:negative?)
+    raise "negative numbers not allowed #{negative_nums.join(', ')}" if negative_nums.any?
+    return numbers.inject(:+)
   end
 end
