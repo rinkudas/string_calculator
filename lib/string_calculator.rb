1dd032404dc0a998c3da9e4a0480276aaf2c0062
Rinku <rinkudas.id@gmail.com>
2024-05-09 20:14:31 +0530

    Added condition to pass test case #6 for string with any amount of numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,11 +1,6 @@
 class StringCalculator
   def add(string)
-    return 0 if string.empty?
-    return string.to_i if string.length == 1
-
-    if string.length >= 2
-      num1, num2 = string.split(',')
-      return num1.to_i + num2.to_i
-    end
+    return 0 if string.empty? || string.nil?
+    return string.split(",").map(&:to_i).inject(:+)
   end
 end
